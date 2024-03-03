539d49abee4f78dc16d0a13f1e8eade2565c8576
Arturo Avila <mysticmetromx@gmail.com>
2024-03-03 14:14:53 -0600

    Added links and reformatted dark mode feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,28 @@
-import Alpine from 'alpinejs';
-import 'htmx.org';
+import Alpine from "alpinejs";
+import "htmx.org";
+import { initFlowbite } from "flowbite";
+
+// Initializing Flowbite.
+initFlowbite();
 
 // Alpine config
 window.Alpine = Alpine;
+
+// Dark mode store.
+Alpine.store("darkMode", {
+  // This just hold a true or false value
+  darkTheme:
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches),
+  // This is a function that toggles the value
+  toggleDarkTheme() {
+    this.darkTheme = !this.darkTheme;
+    localStorage.setItem("color-theme", this.darkTheme ? "dark" : "light");
+  },
+});
+
 Alpine.start();
 
 // HTMX config
-window.htmx = require('htmx.org');
+window.htmx = require("htmx.org");
